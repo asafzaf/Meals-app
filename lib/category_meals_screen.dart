de7209ecfa0e2b5b7d72fd7a9831cb35babda5f0
asafzaf <asaf.zafrir@gmail.com>
2023-03-21 22:32:42 +0200

    turn into pushNamed screen

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CatrgoryMealsScreen extends StatelessWidget {
+  // final String catrgoryId;
+  // final String categoryTitle;
+
+  // CatrgoryMealsScreen({this.catrgoryId, this.categoryTitle});
+
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipes'),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text(
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() {
@@ -24,6 +25,9 @@ class MyApp extends StatelessWidget {
             )),
       ),
       home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) => CatrgoryMealsScreen(),
+      },
     );
   }
 }
