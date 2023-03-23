4eacba8324b63a5f980a74dda7dbb4202a687a16
asafzaf <asaf.zafrir@gmail.com>
2023-03-23 18:19:46 +0200

    added unknown route to main

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,11 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
     );
   }
 }
